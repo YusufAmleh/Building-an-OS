ea8f1eae253a254449b5f790453ec9109a744082
Yusuf Amleh <156696657+YusufAmleh@users.noreply.github.com>
2025-10-29 11:39:58 -0400

    Update main.asm

--- main.asm
+++ main.asm
@@ -11,3 +11,17 @@ main:
 
 times 510-($-$$) db 0 ;times is used to pad the program so that it fills up to 510 bites, after which we declare the two bite signature, in NASM the $ can be used to obtain the assembly position of the current line whereas the $$ gives us the position of the current section, so in this case $-$$ gives us the lenght of the program thus far measured in bites
 dw 0AA55h ;dw is a similar directive to db but it declares  a 2 bite constant which is generally refered to as a word
+
+;bits 16
+;org 0x7C00
+
+;mov ah, 0x0E
+;mov al, 'H'
+;int 0x10
+;mov al, 'i'
+;int 0x10
+
+;jmp $
+
+;times 510 - ($ - $$) db 0
+;dw 0xAA55
